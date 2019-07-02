00ed34a4bd3c9a9a9de31fcd4e1d735644ca7af9
Kazuki Okamoto <kazukiokamoto@iij.ad.jp>
2019-07-02 17:15:29 +0900

    LF

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,15 +1,40 @@
-function Get-SubPsakefile {
-    Get-ChildItem -Filter 'psakefile.ps1' homotuple, homotuple-only, list-tuple, single-tuple
+Properties {
+    $subpackages = "homotuple", "homotuple-only", "list-tuple", "single-tuple"
+    $excludedPaths = "*\.stack-work\*"
+    $crlfFiles = "*.ps1", "TAGS"
 }
 
-function Invoke-SubPsake ($Task) {
+function NotLikeAny-Object ($Target, $Patterns) {
+    $Patterns | ForEach-Object -Begin { $result = $True } -Process { $result = $result -and ($Target -notlike $_) } -End { $result }
+}
+
+function ConvertLF-File ($Path) {
+    $content = Get-Content -Path $Path -Raw
+    if ($content -eq $Null) { $content = "" }
+    $content.Replace("`r`n", "`n") | ForEach-Object { [Text.Encoding]::Default.GetBytes($_) } | Set-Content -Path $Path -Encoding Byte
+}
+
+function Get-Subpsakefile {
+    Get-ChildItem -Filter 'psakefile.ps1' $Subpackages
+}
+
+Function Invoke-Subpsake ($Task) {
     Get-SubPsakefile | ForEach-Object { Invoke-Psake $_.FullName -Task $Task }
 }
 
 Task Format {
-    Exec { Invoke-SubPsake -Task Format }
+    Invoke-Subpsake -Task Format
 }
 
 Task Lint {
-    Exec { Invoke-SubPsake -Task Lint }
+    Invoke-Subpsake -Task Lint
+}
+
+Task Eol {
+    @(Get-ChildItem -File -Exclude ".stack-work" .) + (Get-ChildItem -Recurse -File $subpackages) `
+      | Where-Object { `
+          (NotLikeAny-Object -Target $_.FullName -Patterns $ExcludedPaths) `
+          -and (NotLikeAny-Object -Target $_.Name -Patterns $CrlfFiles) `
+        } `
+      | ForEach-Object { ConvertLF-File -Path $_.FullName }
 }
