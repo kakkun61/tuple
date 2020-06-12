626a50a0fbe3b07e67393c6d6d669bb49fb913b2
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2020-06-12 22:29:21 +0900

    list-tuple 50

--- list-tuple/template/List.hs
+++ list-tuple/template/List.hs
@@ -347,3 +347,73 @@ instance HasAt (a, b) 1
 ---- embed 14
 
 ---- embed 15
+
+---- embed 16
+
+---- embed 17
+
+---- embed 18
+
+---- embed 19
+
+---- embed 20
+
+---- embed 21
+
+---- embed 22
+
+---- embed 23
+
+---- embed 24
+
+---- embed 25
+
+---- embed 26
+
+---- embed 27
+
+---- embed 28
+
+---- embed 29
+
+---- embed 30
+
+---- embed 31
+
+---- embed 32
+
+---- embed 33
+
+---- embed 34
+
+---- embed 35
+
+---- embed 36
+
+---- embed 37
+
+---- embed 38
+
+---- embed 39
+
+---- embed 40
+
+---- embed 41
+
+---- embed 42
+
+---- embed 43
+
+---- embed 44
+
+---- embed 45
+
+---- embed 46
+
+---- embed 47
+
+---- embed 48
+
+---- embed 49
+
+---- embed 50
